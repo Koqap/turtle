a8695b1c77eb3919254c774a7e5f9bced99ffe3c
Arjay Dela Cruz <arjay.delacruz@msugensan.edu.ph>
2025-11-16 15:11:19 +0800

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,11 +1,8 @@
 -- Simple GPS Host Startup
 
 -- SET YOUR COORDINATES HERE
-local x = 0
-local y = 0
-local z = 0
 
 while true do
-    shell.run("gps", "host", x, y, z)
+    shell.run("gps", "host")
     sleep(1)   -- If gps host stops, restart it
 end
